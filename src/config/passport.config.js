49a1674a245cd4d03820520203ee0e61bdec1411
suraj171299 <surajpadalkar1999@gmail.com>
2024-12-31 01:25:29 +0530

    updated google callback url

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -9,7 +9,7 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/api/auth/google/callback",
+        callbackURL: `${process.env.BASE_URL}/api/auth/google/callback`,
         accessType: 'offline',
         prompt: 'consent',
     },
